7874e2d242eec0fca605de3c5e40fd10f62de6be
Keenan Southall <keenan.southall@yahoo.com>
2023-03-01 22:43:04 -0700

    Add food list to display

--- src/Components/AllBeers.js
+++ src/Components/AllBeers.js
@@ -1,21 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../CSS/AllBeers.css'
 
 const AllBeers = (props) => {
     const beers = props.beers.map(beer => {
+
+        console.log(beer['food_pairing'])
+        //console.log(beer)
+
+        const foods = beer['food_pairing'].map(food => {
+            return (
+                <p>{food}</p>
+            )
+        })
+
         return (
             <section>
-                <Link to={`/${beer.name}`}>{beer.name}</Link>
-                <p>{beer.description}</p>
-                <img src={beer['image_url']} alt={beer.name} />
-                <button onClick={() => props.addFavorite(beer.name)}>Add to Favorites</button>
+                <div>
+                    <Link to={`/${beer.name}`} className='title'>{beer.name}</Link>
+                    <img src={beer['image_url']} alt={beer.name} />
+                </div>
+                <div className="text-container">
+                    <p className="text">{beer.description}</p>
+                    <h2>Food Pairings</h2>
+                    {foods}
+                    <button onClick={() => props.addFavorite(beer.name)}>Add to Favorites</button>
+                </div>
             </section>
         )
     })
 
     return (
-        <div >{beers}</div>
+        <div className='beers-container'>{beers}</div>
     )
 }
 
 export default AllBeers
+
+// have a sort feature?
+// abv, aplphabetical, whatever
