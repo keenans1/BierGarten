60e220944e036b3f56880acd0a5fb5978fc9a7b6
Keenan Southall <keenan.southall@yahoo.com>
2023-03-01 22:45:51 -0700

    Remove css import

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router";
+import Header from "./Header";
+import AllBeers from "./AllBeers";
+import Favorites from "./Favorites";
+import SingleBeer from "./SingleBeer";
+//import '../CSS/App.css'
 
 class App extends Component {
   constructor() {
@@ -9,11 +15,37 @@ class App extends Component {
     }
   }
 
+  componentDidMount = () => {
+    fetch('https://api.punkapi.com/v2/beers')
+      .then(response => response.json())
+      .then(data => this.setState({ beers: data }))
+  }
+
+  addFavorite = (newFavorite) => {
+    const find = this.state.favorites.find(favorite => newFavorite === favorite)
+
+    if (find === undefined) {
+      this.setState({ favorites: [...this.state.favorites, newFavorite] })
+    }
+  }
+
   render() {
     return (
-      <div>
-        hello
-      </div>
+      <main>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <h2>Welcome to our site! Check out our menu!</h2>
+          </Route>
+          <Route exact path="/allbeers">
+            <AllBeers beers={this.state.beers} addFavorite={this.addFavorite} />
+          </Route>
+          <Route exact path="/favorites">
+            <Favorites favorites={this.state.favorites} />
+          </Route>
+          <Route path="/:beername" render={({ match }) => <SingleBeer name={match.params.beername} beers={this.state.beers} />} />
+        </Switch>
+      </main>
     )
   }
 }
