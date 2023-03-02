47bea28e2e43eea7834a8beecfd0be87d840f74b
Keenan Southall <keenan.southall@yahoo.com>
2023-03-01 20:22:25 -0700

    Add Link and props

--- src/Components/AllBeers.js
+++ src/Components/AllBeers.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const AllBeers = () => {
+const AllBeers = (props) => {
+    const beers = props.beers.map(beer => {
+        return (
+            <section>
+                <Link to={`/${beer.name}`}>{beer.name}</Link>
+                <p>{beer.description}</p>
+                <img src={beer['image_url']} alt={beer.name} />
+                <button onClick={() => props.addFavorite(beer.name)}>Add to Favorites</button>
+            </section>
+        )
+    })
+
     return (
-        <div> all beers </div>
+        <div >{beers}</div>
     )
 }
 
