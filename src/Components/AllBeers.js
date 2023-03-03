8de2ddab3e9a63a6f71a0e71d7b10559d068df62
Keenan Southall <keenan.southall@yahoo.com>
2023-03-02 17:12:17 -0700

    Add food list

--- src/Components/AllBeers.js
+++ src/Components/AllBeers.js
@@ -5,12 +5,12 @@ import '../CSS/AllBeers.css'
 const AllBeers = (props) => {
     const beers = props.beers.map(beer => {
 
-        console.log(beer['food_pairing'])
+
         //console.log(beer)
 
         const foods = beer['food_pairing'].map(food => {
             return (
-                <p>{food}</p>
+                <li>{food}</li>
             )
         })
 
@@ -19,11 +19,14 @@ const AllBeers = (props) => {
                 <div>
                     <Link to={`/${beer.name}`} className='title'>{beer.name}</Link>
                     <img src={beer['image_url']} alt={beer.name} />
+                    <p>ABV: {beer.abv}</p>
                 </div>
                 <div className="text-container">
                     <p className="text">{beer.description}</p>
                     <h2>Food Pairings</h2>
-                    {foods}
+                    <ul>
+                        {foods}
+                    </ul>
                     <button onClick={() => props.addFavorite(beer.name)}>Add to Favorites</button>
                 </div>
             </section>
