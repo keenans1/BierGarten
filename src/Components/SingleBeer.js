b61ba174464dfe819e0afbfc86e052a90e05a93d
Keenan Southall <keenan.southall@yahoo.com>
2023-03-02 17:13:26 -0700

    Display ingredient list

--- src/Components/SingleBeer.js
+++ src/Components/SingleBeer.js
@@ -3,17 +3,33 @@ import React from 'react'
 const SingleBeer = (props) => {
     const foundBeer = props.beers.find(beer => beer.name === props.name)
 
-    let foundName
-
     if (foundBeer) {
-        foundName = foundBeer.name
-    }
+        const { name, description, ingredients, ph } = foundBeer
 
-    return (
-        <section>
-            <h2>using find {foundName}</h2>
-        </section>
-    )
+        console.log(ingredients)
+
+        const hopsList = ingredients.hops.map(hop => <li>{hop.name}</li>)
+        const maltList = ingredients.malt.map(malt => <li>{malt.name}</li>)
+
+        return (
+            <section>
+                <h2>{name}</h2>
+                <p>{description}</p>
+                <p>{ph}</p>
+                <ul>
+                    {hopsList}
+                </ul>
+                <ul>
+                    {maltList}
+                </ul>
+                <p>yeast {ingredients.yeast}</p>
+            </section>
+        )
+    }
 }
 
 export default SingleBeer
+
+// hops array
+// malt array
+// yeast string
