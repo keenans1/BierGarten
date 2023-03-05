014c78cc34081dbcc320e6d27c643f7732be2c72
Keenan Southall <keenan.southall@yahoo.com>
2023-03-04 20:11:10 -0700

    Create Home component to render home display info

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <section className="info-text-container">
+            <h2>Welcome to the BierGarten!</h2>
+            <h2> For information about all of our beers, click the beers tab in the menu</h2>
+        </section>
+    )
+}
+
+export default Home
