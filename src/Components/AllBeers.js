635d3ecb499259bdaca35d9a90f91e255fc4017e
Keenan Southall <keenan.southall@yahoo.com>
2023-03-04 21:44:56 -0700

    Fix key props issue

--- src/Components/AllBeers.js
+++ src/Components/AllBeers.js
@@ -3,16 +3,17 @@ import { Link } from "react-router-dom";
 import '../CSS/AllBeers.css'
 
 const AllBeers = (props) => {
-    const beers = props.beers.map(beer => {
 
-        const foods = beer['food_pairing'].map(food => {
+    const beers = props.beers.map((beer, index) => {
+
+        const foods = beer['food_pairing'].map((food, index) => {
             return (
-                <li>{food}</li>
+                <li key={index}>{food}</li>
             )
         })
 
         return (
-            <section>
+            <section key={index}>
                 <div>
                     <Link to={`/beers/${beer.id}`} className='title'>
                         <p>{beer.name}</p>
@@ -21,11 +22,8 @@ const AllBeers = (props) => {
                     <p>ABV: {beer.abv}</p>
                 </div>
                 <div className="text-container">
-                    {/* <p className="text">{beer.description}</p> */}
                     <h2>Food Pairings</h2>
-                    <ul>
-                        {foods}
-                    </ul>
+                    <ul>{foods}</ul>
                     <button onClick={() => props.addFavorite(beer.name)}>Add to Favorites</button>
                     <p>Click the title or image to see more info about {beer.name}</p>
                 </div>
@@ -41,4 +39,4 @@ const AllBeers = (props) => {
 export default AllBeers
 
 // have a sort feature?
-// abv, aplphabetical, whatever
+// abv, aplphabetical
