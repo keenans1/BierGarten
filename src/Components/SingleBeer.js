6c94db978001f4c0ac7b6e310570263a676fac8c
Keenan Southall <keenan.southall@yahoo.com>
2023-03-04 17:43:00 -0700

    Refactor SingleBeer to class component and implement fetch call

--- src/Components/SingleBeer.js
+++ src/Components/SingleBeer.js
@@ -1,35 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const SingleBeer = (props) => {
-    const foundBeer = props.beers.find(beer => beer.name === props.name)
+class SingleBeer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            beer: {
 
-    if (foundBeer) {
-        const { name, description, ingredients, ph } = foundBeer
+            }
+        }
+    }
 
-        console.log(ingredients)
+    componentDidMount = () => {
+        fetch(`https://api.punkapi.com/v2/beers/${this.props.id}`)
+            .then(response => response.json())
+            .then(data => this.setState({ beer: data[0] }))
+    }
 
-        const hopsList = ingredients.hops.map(hop => <li>{hop.name}</li>)
-        const maltList = ingredients.malt.map(malt => <li>{malt.name}</li>)
+    render() {
+        if (this.state.beer.name) {
 
-        return (
-            <section>
-                <h2>{name}</h2>
-                <p>{description}</p>
-                <p>{ph}</p>
-                <ul>
-                    {hopsList}
-                </ul>
-                <ul>
-                    {maltList}
-                </ul>
-                <p>yeast {ingredients.yeast}</p>
-            </section>
-        )
+            const { name, description, ph, ingredients } = this.state.beer
+            const hopsList = ingredients.hops.map(hop => <li>{hop.name}</li>)
+            const maltList = ingredients.malt.map(malt => <li>{malt.name}</li>)
+
+            return (
+                <section className='info-container'>
+                    <h2>Description: {name}</h2>
+                    <p>{description}</p>
+                    <p>{ph}</p>
+                    <ul>
+                        hops {hopsList}
+                    </ul>
+                    <ul>
+                        malt {maltList}
+                    </ul>
+                    <p>yeast {ingredients.yeast}</p>
+                </section>
+            )
+        }
     }
 }
 
 export default SingleBeer
-
-// hops array
-// malt array
-// yeast string
