b14a713774d7e2fea4c0ff5955e30c60fce343f7
Keenan Southall <keenan.southall@yahoo.com>
2023-03-04 20:12:08 -0700

    Refactor to check for response error

--- src/APIFetchCall.js
+++ src/APIFetchCall.js
@@ -1,6 +1,11 @@
 const fetchBeerData = (beerID = '') => {
     return fetch(`https://api.punkapi.com/v2/beers/${beerID}`)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Something went wrong')
+            }
+            return response.json()
+        })
 }
 
 export default fetchBeerData 
