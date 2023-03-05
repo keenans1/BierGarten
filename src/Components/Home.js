eef338d14d43da8d30c1c3e1c620fca6bc874e03
Keenan Southall <keenan.southall@yahoo.com>
2023-03-05 13:08:54 -0700

    Update class name

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import '../CSS/Home.css';
 
 const Home = () => {
     return (
-        <section className="info-text-container">
+        <section className="home-text-container">
             <h2>Welcome to the BierGarten!</h2>
-            <h2> For information about all of our beers, click the beers tab in the menu</h2>
+            <h2>For information about all of our beers, click the beers tab in the menu</h2>
         </section>
     )
 }
