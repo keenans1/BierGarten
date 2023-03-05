1d41f43a87d41cf3e66ce64894542fd5312b8de5
Keenan Southall <keenan.southall@yahoo.com>
2023-03-04 18:54:44 -0700

    Refactor fetchBeerData function

--- src/APIFetchCall.js
+++ src/APIFetchCall.js
@@ -1,9 +1,6 @@
-fetchBeerData = (beerID) => {
-
-    const endpoint = `/${beerID}` || ''
-
-    return fetch(`https://api.punkapi.com/v2/beers${endpoint}`)
+const fetchBeerData = (beerID = '') => {
+    return fetch(`https://api.punkapi.com/v2/beers/${beerID}`)
         .then(response => response.json())
 }
 
-export default fetchBeerData
+export default fetchBeerData 
