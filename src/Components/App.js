dcdaf5ae63ad2d276e215cd5001d9ec61d60ed03
Keenan Southall <keenan.southall@yahoo.com>
2023-03-04 20:11:43 -0700

    Refactor Routes to implement Home and Error components

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,6 +4,8 @@ import Header from "./Header";
 import AllBeers from "./AllBeers";
 import Favorites from "./Favorites";
 import SingleBeer from "./SingleBeer";
+import Error from "./Error";
+import Home from "./Home";
 import '../CSS/App.css';
 import fetchBeerData from "../APIFetchCall";
 
@@ -34,20 +36,20 @@ class App extends Component {
       <main>
         <Header />
         <Switch>
-          <Route exact path="/">
-            <section className="info-text-container">
-              <h2>Welcome to the BierGarten!</h2>
-              <h2> For information about all of our beers, click the beers tab in the menu</h2>
-              {/* <h2>If you would like to favorite a beer, simply click the add to favorite button on your desired brew</h2> */}
-            </section>
-          </Route>
-          <Route exact path="/allbeers">
+          <Route exact path="/" render={() => <Home />} />
+          {/* <Route exact path="/allbeers">
             <AllBeers beers={this.state.beers} addFavorite={this.addFavorite} />
-          </Route>
-          <Route exact path="/favorites">
+          </Route> */}
+          <Route exact path="/allbeers" render={() => <AllBeers beers={this.state.beers} addFavorite={this.addFavorite} />} />
+
+          {/* <Route exact path="/favorites">
             <Favorites favorites={this.state.favorites} beers={this.state.beers} />
-          </Route>
+          </Route> */}
+
+          <Route exact path="/favorites" render={() => <Favorites favorites={this.state.favorites} beers={this.state.beers} />} />
+
           <Route path="/beers/:beerid" render={({ match }) => <SingleBeer id={match.params.beerid} />} />
+          <Route path="/:error" render={() => <Error />} />
         </Switch>
       </main>
     )
