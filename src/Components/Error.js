c26b4ea2cb689d622656f44dffba93944591a4ae
Keenan Southall <keenan.southall@yahoo.com>
2023-03-05 18:02:48 -0700

    Add id to error message

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -3,7 +3,7 @@ import React from "react";
 const Error = () => {
     return (
         <section>
-            <p>It looks like this url is invalid</p>
+            <p id='errorMessage'>It looks like this url is invalid</p>
             <p>Please click an option in the menu bar to go to a valid url</p>
         </section>
     )
