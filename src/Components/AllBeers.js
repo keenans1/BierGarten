d14d416d019dd62d575872bd7c67f9a24bd0f656
Keenan Southall <keenan.southall@yahoo.com>
2023-03-05 19:01:19 -0700

    Add Prop Types

--- src/Components/AllBeers.js
+++ src/Components/AllBeers.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../CSS/AllBeers.css';
+import PropTypes from 'prop-types';
 
 const AllBeers = (props) => {
+
+    console.log('all beers', props)
 
     const beers = props.beers.map((beer, index) => {
 
@@ -36,6 +39,106 @@ const AllBeers = (props) => {
 }
 
 export default AllBeers
+
+AllBeers.propTypes = {
+    beers: PropTypes.arrayOf(
+        PropTypes.shape(
+            {
+                abv: PropTypes.number,
+                attenuation_level: PropTypes.number,
+                boil_volume: PropTypes.shape(
+                    {
+                        value: PropTypes.number,
+                        unit: PropTypes.string
+                    }
+                ),
+                brewers_tips: PropTypes.string,
+                contributed_by: PropTypes.string,
+                description: PropTypes.string,
+                ebc: PropTypes.number,
+                first_brewed: PropTypes.string,
+                food_pairing: PropTypes.arrayOf(PropTypes.string),
+                ibu: PropTypes.number,
+                id: PropTypes.number,
+                image_url: PropTypes.string,
+                ingredients: PropTypes.shape(
+                    {
+                        malt: PropTypes.arrayOf(
+                            PropTypes.shape(
+                                {
+                                    amount: PropTypes.shape(
+                                        {
+                                            value: PropTypes.number,
+                                            unit: PropTypes.string
+                                        }
+                                    ),
+                                    name: PropTypes.string
+                                }
+                            )
+                        ),
+                        hops: PropTypes.arrayOf(
+                            PropTypes.shape(
+                                {
+                                    add: PropTypes.string,
+                                    amount: PropTypes.shape(
+                                        {
+                                            value: PropTypes.number,
+                                            unit: PropTypes.string
+                                        }
+                                    ),
+                                    attribute: PropTypes.string,
+                                    name: PropTypes.string
+                                }
+                            )
+                        ),
+                        yeast: PropTypes.string
+                    }
+                ),
+                method: PropTypes.shape(
+                    {
+                        fermentation: PropTypes.shape(
+                            {
+                                temp: PropTypes.shape(
+                                    {
+                                        value: PropTypes.number,
+                                        unit: PropTypes.string
+                                    }
+                                )
+                            }
+                        ),
+                        mash_temp: PropTypes.arrayOf(
+                            PropTypes.shape(
+                                {
+                                    duration: PropTypes.number,
+                                    temp: PropTypes.shape(
+                                        {
+                                            value: PropTypes.number,
+                                            unit: PropTypes.string
+                                        }
+                                    )
+                                }
+                            )
+                        ),
+                        twist: PropTypes.any
+                    }
+                ),
+                name: PropTypes.string,
+                ph: PropTypes.number,
+                srm: PropTypes.number,
+                tagline: PropTypes.string,
+                target_fg: PropTypes.number,
+                target_og: PropTypes.number,
+                volume: PropTypes.shape(
+                    {
+                        value: PropTypes.number,
+                        unit: PropTypes.string
+                    }
+                )
+            }
+        )
+    ),
+    addFavorite: PropTypes.func
+}
 
 // have a sort feature?
 // abv, aplphabetical
